9f50fc19f2c4c2d0425f978795c652ec3fb11bbd
marketmoversbot-ui <marketmoversbot@gmail.com>
2026-04-28 17:51:25 -0500

    Add temporary homepage to fix build

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40,fontFamily:'Arial'}}>
+      <h1>Bay Area Wash Bros</h1>
+      <p>Site is deploying. Full version loading shortly.</p>
+    </main>
+  );
+}
